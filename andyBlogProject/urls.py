d427f6449c7a1747653207d6f35de8c1642a6bc1
AndyLeeJP <rize1126jesse@gmail.com>
2023-03-26 20:34:21 +0900

    create screen for posts

--- andyBlogProject/urls.py
+++ andyBlogProject/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from andyBlogApp.views import frontpage
+from andyBlogApp.views import frontpage, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", frontpage)
+    path("", frontpage),
+    path("<slug:slug>/", post_detail, name="post_detail")
 ]
