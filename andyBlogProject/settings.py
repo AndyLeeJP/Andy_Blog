4ec44e07bce288037eb2a34c7c7f5cf0fd60fbfb
AndyLeeJP <rize1126jesse@gmail.com>
2023-03-28 16:04:50 +0900

    connect css

--- andyBlogProject/settings.py
+++ andyBlogProject/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# PROJECT_NAME = os.path.basename(BASE_DIR)
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
+# STATIC_ROOT = BASE_DIR, 'staticfiles'
+# STATICFILES_DIRS = (BASE_DIR / 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
